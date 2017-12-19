8e3723ce80cfbef86b25bf1c1157ac073797e422
Alize Nguyen <alizenguyen@gmail.com>
2017-12-18 21:24:13 -0600

    difference logic added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,6 @@
 
 var friendsData = require("../data/friend");
 
-console.log(friendsData);
 // ===============================================================================
 // ROUTING
 // ===============================================================================
@@ -23,7 +22,39 @@ module.exports = function(app) {
   app.post("/api/friend", function(req, res) {
     
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    var newUserScore = req.body.scores;
+    var results = [];
+    var match = 0;
+    var besties;
 
+    //pushes user to friendsArray
+    friendsData.push(req.body);
+
+    console.log(friendsData)
+    
+    //Goes through friendsArray, compares scores, and pushes the difference of each comparison to results array
+    for (var i=0; i<friendsData.length; i+=1) {
+      var difference = 0;
+      //Goes through newUserScore array and compares each score with each individual user in the array
+      for (var j=0; j<newUserScore.length; i+=1) {
+        difference += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newUserScore[j])))
+      };
+      //pushes to results array
+      results.push(difference);
+    }
+
+    //Goes through the results array
+    for (var i=0; i<results.length; i+=1) {
+      //Compares if the difference is less than or equal to the first result index, if less or equal match gets reassigned and compared again until loop is complete
+      if(results[i] <= results[match]) {
+        match = i
+      }
+    };
+
+    //new variable get assigned the best match and gets sent out
+    besties = friendsData[bestMatch]; 
+    console.log(besties);
+    res.json(besties);
   });
 
 };
